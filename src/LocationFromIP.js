21a78b9600af775ff4f866b9243e93a1b448e369
Aiden Brook <amonydollersign@Aidens-MacBook-Pro.local>
2022-02-08 10:11:40 -0500

    lab4/5 opt 2

--- src/LocationFromIP.js
+++ src/LocationFromIP.js
@@ -20,8 +20,8 @@ export class LocationFromIP extends LitElement {
     return {
       lat: { type: Number },
       long: { type: Number },
-      city: { type: String },
-      regionName: { type: String },
+      city: {type: String },
+      regionName: {type: String}
     };
   }
 
@@ -71,23 +71,24 @@ export class LocationFromIP extends LitElement {
     // this means you can make new variables and then bind them this way if you like
     const url = `https://maps.google.com/maps?q=${this.lat},${this.long}&t=&z=15&ie=UTF8&iwloc=&output=embed`;
     return html`<iframe title="Where you are" src="${url}"></iframe>
-
+      
       <ul>
-        <a href="https://maps.google.com/maps?q=@${this.lat},${this.long},14z">
-          Open in maps
-        </a>
+      <a href = "https://maps.google.com/maps?q=@${this.lat},${this.long},14z">
+        Open in maps
+      </a>
       </ul>
-
+      
       <script>
-        window.__appCDN = 'https://cdn.webcomponents.psu.edu/cdn/';
+      window.__appCDN="https://cdn.webcomponents.psu.edu/cdn/";
       </script>
-      <script src="https://cdn.webcomponents.psu.edu/cdn/build.js"></script>
+      <script src="https://cdn.webcomponents.psu.edu/cdn/build.js">
+      </script>
 
       <wikipedia-query search="${this.city}"></wikipedia-query>
       <wikipedia-query search="${this.region}"></wikipedia-query>
-      <wikipedia-query
-        search="${this.city},${this.region}"
-      ></wikipedia-query> `;
+      <wikipedia-query search="${this.city},${this.region}"></wikipedia-query>
+      `;
+      
   }
 }
 
---
+++ src/nasaIMG.js
@@ -0,0 +1,112 @@
+import { LitElement, html, css } from 'lit';
+import '@lrnwebcomponents/accent-card/accent-card.js';
+
+export class nasaIMG extends LitElement{
+    
+    constructor(){
+        super();
+        this.view = 'card';
+        this.image = [];
+        this.loadData = false;
+        this.page = 1;
+    }
+
+    static getProperties(){
+        return{
+            view: {type:String, reflect: true},
+            images: {type: Array},
+            loadData: {type: Boolean, reflect: true, attribute: 'loadData',},
+            page: {type: Number, reflect: true}
+        }
+    }
+
+    updated(changedProperties) {
+        changedProperties.forEach((oldValue, propName) => {
+          if (propName === 'loadData' && this[propName]) {
+            this.getData();
+          }
+          // when dates changes, fire an event for others to react to if they wish
+          else if (propName === 'images') {
+            this.dispatchEvent(
+              new CustomEvent('results-changed', {
+                detail: {
+                  value: this.images,
+                },
+              })
+            );
+          }
+        });
+      }
+
+      getData(){
+          fetch(`https://images-api.nasa.gov/search?q=${this.name}&media_type=image&page=${this.page}`)
+          .then(resp => {
+            if (resp.ok) {
+              return resp.json();
+            }
+            return false;
+          })
+          .then(data => {
+            this.images = [];
+            for (let i = 0; i < data.length; i++) {
+                const eventInfo = {
+                  name: data[i].title,
+                  description: data[i].description,
+                  page: data[i].page,
+                  photographer: data[i].photographer,
+                  secCreator: data[i].secondary_creator,
+                };
+                this.data.push(eventInfo);
+            }
+
+          });
+      }
+
+      resetData(){
+          this.images = [];
+          this.loadData = false;
+      }
+
+      static get styles() {
+        return css`
+          :host {
+            display: block;
+            border: 2px solid black;
+            min-height: 100px;
+          }
+          :host([view='list']) ul {
+            margin: 20px;
+          }
+        `;
+      }
+
+      render(){
+        return html`
+            ${this.view === 'list'
+            ? html`
+              <ul>
+                ${this.images.map(
+                  item => html`
+                    <li>
+                      ${item.image} - ${item.name} - ${item.description} ${item.photographer}
+                      - ${item.secCreator}
+                    </li>
+                  `
+                )}
+              </ul>
+              `
+        : html`
+            ${this.images.map(
+              item => html`
+                <accent-card image-src="${item.image}" accent-color="light-blue" style="max-width:600px;">
+                <div slot="heading">${item.name}</div>
+                <div slot="content">${item.description}</div>
+                </accent-card>
+              `
+            )}
+          `}
+    `;
+  }
+
+}
+customElements.define('nasa-image-search', nasaIMG);
